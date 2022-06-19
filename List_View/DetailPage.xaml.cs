9bd697475848be001207af498004356c8e0294e1
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-06-19 18:55:40 +0530

    First Commit

---
+++ List_View/DetailPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace List_View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage( string ImageLink, string Name, string Address)
+        {
+            InitializeComponent();
+            EmpName.Text = Name;
+            EmpAddress.Text = Address;
+            EmpImage.Source = ImageLink;
+        }
+    }
+}
---
+++ List_View/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace List_View
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ImageLink { get; set; }
+    }
+    public partial class MainPage 
+    {
+        public List<Employee> emp = new List<Employee>
+        {
+            new Employee{Name="Vinit", Address="Mumbai", ImageLink="Boy.png"},
+            new Employee{Name="Durga", Address="Hydrabad", ImageLink="Girl.png"},
+            new Employee{Name="Lohit", Address="Banglore", ImageLink="Boy.png"}
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            // Normal List View
+            simpleCityList.ItemsSource = new List<string>
+            {
+                "Mumbai","Pune","Hydrabad"
+            };
+
+            // Data Binding & Item Binding in View Cell
+            viewCellEmpList.ItemsSource = emp;
+
+            // Text Cell Data Bind
+            textCellEmpList.ItemsSource = emp;
+
+            // Image Cell Data Bind
+            imageCellEmpList.ItemsSource = emp;
+
+            // Get Data From Selected Item of List
+            getDataEmpList.ItemsSource = emp;
+        }
+
+        private void clicked_ListView(object sender, ItemTappedEventArgs e)
+        {
+            var detail = e.Item as Employee;
+            Navigation.PushAsync(new DetailPage(detail.ImageLink, detail.Name, detail.Address));
+        }
+    }
+}
